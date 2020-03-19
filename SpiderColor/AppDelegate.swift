055fa95b8fb45e8e4fae321e69131e360ef93c1b
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2020-03-19 00:40:38 -0300

    Add pod 'Firebase/Analytics'

--- SpiderColor/AppDelegate.swift
+++ SpiderColor/AppDelegate.swift
@@ -6,17 +6,15 @@
 //  Copyright © 2020 Veevaz. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool { FirebaseApp.configure()
         return true
     }
 
@@ -36,7 +34,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
-
 }
-
