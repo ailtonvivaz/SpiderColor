94f547ee3990c38cdd54f14de065b7f4fa0b7d40
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2020-04-11 22:26:03 -0300

    Add SplashViewController

---
+++ SpiderColor/Controller/Splash/SplashViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SplashViewController.swift
+//  SpiderColor
+//
+//  Created by Ailton Vieira Pinto Filho on 11/04/20.
+//  Copyright © 2020 Veevaz. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
